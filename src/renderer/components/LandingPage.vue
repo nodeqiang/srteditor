541c28a5e9d744989c136c36ccaa7b933571c4ce
wesleywang <9610848@qq.com>
2019-10-17 18:01:33 +0800

    capture

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -3,52 +3,48 @@
        style='width:100vwheight:100vh'
        class='d-flex flex-column align-items-center justify-content-center'>
        <div :style="{left, top}" style="z-index:100;width:10px;height:10px;background-color:green;position:absolute;" />
-       <b-img :class="{active: activeSlide === slide}" v-for="(slide, idx) in sortedslider" :key="idx" :src="slide.img" :style="{width: `${slide.width * 2 / 3}px`, height: `${slide.height * 2 / 3}px`}" style="position:absolute;left:10px;bottom:10px;" />
-    <b-button @click='startrecord' class='m-5'>开始录制</b-button>
-    <b-button @click='stoprecord' class='m-5'>停止录制</b-button>
-    <b-button style="" class='m-5'>拖动</b-button>
+       <b-img :id="`img_${idx}`" @drag="imgdraging($event, slide, idx)" @dragstart="imgdragstart($event, slide, idx)" @dragend="imgdragstop($event, slide)" v-for="(slide, idx) in sortedslider" :key="idx" :src="slide.img" :style="{width: `${slide.width}px`, height: `${slide.height}px`, left: `${slide.left}px`, bottom: `${slide.bottom}px`, zIndex: slide.zIndex || 0}" style="position:absolute;" />
+    <div>
+      <b-button @click='startrecord' class='m-5'>开始录制</b-button>
+      <b-button @click='stoprecord' class='m-5'>停止录制</b-button>
+      <b-button @click='startcapture' class='m-5'>开始捕获</b-button>
+      <b-button @click='stopcapture' class='m-5'>停止捕获</b-button>
+      <b-button @click='savecapture' class='m-5'>导出视频</b-button>
+    </div>
 
     <span>{{position}}</span>
-    <canvas id='preview' style='position:absolute;right:10px;bottom:10px;width:1280px;height:720px;border:1px solid black' />
+    <canvas width="1920" height="1080" id='preview' style='position:absolute;left:10px;bottom:10px;width:1920px;height:1080px;border:1px solid black' />
+    <canvas width="1920" height="1080" @mouseup="stopDraw" @mousemove="drawLine" @mousedown="startDraw" id='cover' :style="{display: coverdisplay}" style='position:absolute;left:10px;bottom:10px;width:1920px;height:1080px;border:1px solid black;z-index:10000;' />
   </div>
 </template>
 
 <script>
-/* global Leap */
 const { ipcRenderer } = require('electron')
-let updatefun
-Leap.loop({
-  enableGestures: true,
-  hand: function (hand) {
-    // console.log(hand.screenPosition())
-    updatefun && updatefun(hand.screenPosition())
-  }
-}, (frame) => {
-  if (frame.gestures.length > 0) {
-    // console.log(frame.gestures)
-  }
-}).use('screenPosition')
+const { ImageLayer, CanvasLayer, Capture } = require('./capture')
+
 export default {
   data: () => ({
     position: '',
     left: 0,
     top: 0,
     activeSlide: null,
+    maxZIndex: 0,
+    coverdisplay: 'inline',
     slides: [
-      {img: '/static/images/img1.png', width: 1145, height: 809},
-      {img: '/static/images/img2.png', width: 1140, height: 601},
-      {img: '/static/images/img3.png', width: 1136, height: 515},
-      {img: '/static/images/img4.png', width: 1144, height: 692},
-      {img: '/static/images/img5.png', width: 1131, height: 759},
-      {img: '/static/images/img6.png', width: 1136, height: 771},
-      {img: '/static/images/img7.png', width: 1133, height: 1005},
-      {img: '/static/images/img8.png', width: 1132, height: 980},
-      {img: '/static/images/img9.png', width: 1127, height: 979},
-      {img: '/static/images/img10.png', width: 1135, height: 988},
-      {img: '/static/images/img11.png', width: 1133, height: 1006},
-      {img: '/static/images/img12.png', width: 1143, height: 990},
-      {img: '/static/images/img13.png', width: 1135, height: 976},
-      {img: '/static/images/img14.png', width: 1133, height: 1010}
+      {img: '/static/images/img1.png', width: 1145, height: 809, left: 1940, bottom: 10, zIndex: 0},
+      {img: '/static/images/img3.png', width: 1136, height: 515, left: 1940, bottom: 10, zIndex: 0},
+      {img: '/static/images/img2.png', width: 1140, height: 601, left: 1940, bottom: 10, zIndex: 0},
+      {img: '/static/images/img4.png', width: 1144, height: 692, left: 1940, bottom: 10, zIndex: 0},
+      {img: '/static/images/img5.png', width: 1131, height: 759, left: 1940, bottom: 10, zIndex: 0},
+      {img: '/static/images/img6.png', width: 1136, height: 771, left: 1940, bottom: 10, zIndex: 0},
+      {img: '/static/images/img7.png', width: 1133, height: 1005, left: 1940, bottom: 10, zIndex: 0},
+      {img: '/static/images/img8.png', width: 1132, height: 980, left: 1940, bottom: 10, zIndex: 0},
+      {img: '/static/images/img9.png', width: 1127, height: 979, left: 1940, bottom: 10, zIndex: 0},
+      {img: '/static/images/img10.png', width: 1135, height: 988, left: 1940, bottom: 10, zIndex: 0},
+      {img: '/static/images/img11.png', width: 1133, height: 1006, left: 1940, bottom: 10, zIndex: 0},
+      {img: '/static/images/img12.png', width: 1143, height: 990, left: 1940, bottom: 10, zIndex: 0},
+      {img: '/static/images/img13.png', width: 1135, height: 976, left: 1940, bottom: 10, zIndex: 0},
+      {img: '/static/images/img14.png', width: 1133, height: 1010, left: 1940, bottom: 10, zIndex: 0}
     ]
   }),
   computed: {
@@ -56,14 +52,86 @@ export default {
       return this.slides.reverse()
     }
   },
-  created () {
-    updatefun = (pos) => {
-      this.position = `${Math.round(pos[0])},${Math.round(pos[1])},${Math.round(pos[2])}`
-      this.left = `${pos[0]}px`
-      this.top = `${window.innerHeight / 2 + pos[1]}px`
-    }
+  mounted () {
+    this.capture = new Capture({width: 1920, height: 1080})
+    this.slides.forEach((slider, idx) => {
+      const id = `img_${idx}`
+      const elem = document.getElementById(id)
+      const target = document.getElementById('preview')
+      const left = elem.offsetLeft - target.offsetLeft
+      const top = elem.offsetTop - target.offsetTop
+      slider.layer = this.capture.addLayer(new ImageLayer(0, slider.img, left, top, slider.width, slider.height))
+    })
+    this.canvasLayer = this.capture.addLayer(new CanvasLayer(10000, 0, 0, 1920, 1080))
+    this.canvas = document.getElementById('cover')
+    this.context = this.canvas.getContext('2d')
+    this.context.lineWidth = 5
+    this.context.strokeStyle = '#ff0000'
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'c') {
+        console.log('clear')
+        this.coverdisplay = 'none'
+      } else if (e.key === 'd') {
+        this.coverdisplay = 'inline'
+        console.log('draw')
+      }
+    })
   },
   methods: {
+    startDraw (e) {
+      this.lineStart = [e.offsetX, e.offsetY]
+      this.context.beginPath()
+      this.context.moveTo(e.offsetX, e.offsetY)
+      this.canvasLayer.move([e.offsetX, e.offsetY])
+    },
+    stopDraw (e) {
+      this.lineStart = null
+    },
+    drawLine (e) {
+      if (this.lineStart) {
+        this.context.lineTo(e.offsetX, e.offsetY)
+        this.context.stroke()
+        this.canvasLayer.line([e.offsetX, e.offsetY])
+      }
+    },
+    imgdragstart (e, slider, idx) {
+      e.dataTransfer.setDragImage(e.target, e.offsetX, e.offsetY)
+      this.startPos = [e.offsetX, e.offsetY]
+
+      const id = `img_${idx}`
+      const elem = document.getElementById(id)
+      const target = document.getElementById('preview')
+      const left = elem.offsetLeft - target.offsetLeft
+      const top = elem.offsetTop - target.offsetTop
+      this.startOffset = [left, top]
+    },
+    imgdraging (e, slider, idx) {
+      if (e.screenX === 0) return
+      const left = this.startOffset[0] + e.offsetX - this.startPos[0]
+      const top = this.startOffset[1] + e.offsetY - this.startPos[1]
+      slider.layer.setPos(left, top)
+      slider.layer.setZIndex(this.maxZIndex + 1)
+    },
+    imgdragstop (e, slider) {
+      slider.left += e.offsetX - this.startPos[0]
+      slider.bottom -= e.offsetY - this.startPos[1]
+      slider.zIndex = ++this.maxZIndex
+    },
+    startcapture () {
+      this.capture.start()
+    },
+    stopcapture () {
+      this.capture.stop()
+    },
+    savecapture () {
+      this.capture.save((frame, ext, buffer) => {
+        console.log(frame, ext)
+        const fs = require('fs')
+        const path = `/Users/wesleywang/Desktop/dump/${frame}.${ext}`
+        fs.writeFileSync(path, buffer)
+        return path
+      })
+    },
     stoprecord () {
       ipcRenderer.sendSync('stoprecord')
       this.previewing = false
@@ -74,7 +142,6 @@ export default {
       const image = new Image()
       this.previewing = true
       let imgpath = ipcRenderer.sendSync('startrecord')
-      console.log(imgpath)
       image.onload = () => {
         context.drawImage(image, 0, 0, canvas.width, canvas.height)
         if (this.previewing) image.src = `file://${ipcRenderer.sendSync('preview')}`
@@ -105,4 +172,9 @@ export default {
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+.active {
+  border: 3px solid red;
+}
+
 </style>
---
+++ src/renderer/components/capture.js
@@ -0,0 +1,195 @@
+class Layer {
+  constructor (zindex, left, top, width, height) {
+    this.zindex = zindex
+    this.target = null
+    this.left = left
+    this.top = top
+    this.width = width
+    this.height = height
+    this.startTick = new Date().getTime()
+    this.operations = []
+  }
+  saveOperation (func) {
+    this.operations.push({
+      tick: new Date().getTime(),
+      func
+    })
+  }
+  intersect (boundrect) {
+    const nonIntersect = (this.left + this.width < boundrect.left) ||
+      (this.left > boundrect.right) ||
+      (this.top + this.height < boundrect.top) ||
+      (this.top > boundrect.bottom)
+    return !nonIntersect
+  }
+  setZIndex (zindex) {
+    this.saveOperation(() => {
+      this.zindex = zindex
+    })
+  }
+  update (tick) {
+    this._reset()
+    let i
+    for (i = 0; i < this.operations.length; i++) {
+      const otick = this.operations[i].tick
+      if (otick > tick) break
+      this.operations[i].func()
+    }
+    return i
+  }
+  _reset () { }
+  clear () {
+    this.saveOperation(() => {
+      this._reset()
+    })
+  }
+  draw (context) {
+    if (this.target) context.drawImage(this.target, this.left, this.top, this.width, this.height)
+  }
+}
+
+export class ImageLayer extends Layer {
+  constructor (zindex, url, left, top, width, height) {
+    super(zindex, left, top, width, height)
+    this.url = url
+    this.target = new Image()
+    this.target.src = url
+    this._reset = () => {
+      this.left = left
+      this.top = top
+    }
+  }
+  setPos (left, top) {
+    this.saveOperation(() => {
+      this.left = left
+      this.top = top
+    })
+  }
+}
+
+export class CanvasLayer extends Layer {
+  constructor (zindex, left, top, width, height) {
+    super(zindex, left, top, width, height)
+    this.target = document.createElement('canvas')
+    this.target.width = width
+    this.target.height = height
+    this.context = this.target.getContext('2d')
+    this.context.lineWidth = 5
+    this.context.strokeStyle = '#ff0000'
+  }
+  _reset () {
+    this.context.clearRect(0, 0, this.target.width, this.target.height)
+  }
+  move (to) {
+    this.saveOperation(() => {
+      this.context.beginPath()
+      this.context.moveTo(...to)
+    })
+  }
+  line (to) {
+    this.saveOperation(() => {
+      this.context.lineTo(...to)
+      this.context.stroke()
+    })
+  }
+}
+
+export class Capture {
+  constructor (opts) {
+    opts = opts || {}
+    this.framerate = opts.framerate || 30
+    this.width = opts.width || 1920
+    this.height = opts.height || 1080
+    this.layers = []
+  }
+  start () {
+    this.startTick = new Date().getTime()
+    this.layers.forEach(layer => { layer.operations = [] })
+  }
+  addLayer (layer) {
+    this.layers.push(layer)
+    return layer
+  }
+  stop () {
+    this.stopTick = new Date().getTime()
+  }
+  save (savefunc) {
+    const framemap = {}
+    const frames = []
+    for (let frame = 1; ; frame++) {
+      const tick = this.startTick + 1000 * frame / this.framerate
+      if (tick > this.stopTick) {
+        break
+      }
+      const canvas = document.createElement('canvas')
+      canvas.width = this.width
+      canvas.height = this.height
+      const boundrect = {left: 0, top: 0, right: this.width, bottom: this.height}
+      const context = canvas.getContext('2d')
+      const layerdeep = JSON.stringify(this.layers.map(layer => layer.update(tick)))
+      if (framemap[layerdeep]) {
+        // console.log('skip save frame', frame, 'same as', framemap[layerdeep])
+        frames[frames.length - 1].duration++
+        continue
+      }
+      this.layers.sort((a, b) => a.zindex - b.zindex)
+      this.layers.forEach(layer => {
+        if (layer.intersect(boundrect)) {
+          layer.draw(context)
+        }
+      })
+
+      const string = canvas.toDataURL()
+      const regex = /^data:.+\/(.+);base64,(.*)$/
+      const matches = string.match(regex)
+      const ext = matches[1]
+      const data = matches[2]
+      const buffer = Buffer.from(data, 'base64')
+
+      const path = savefunc(frame, ext, buffer)
+      frames.push({ frame, duration: 1, path })
+      framemap[layerdeep] = frame
+    }
+    this.exportfcp(savefunc, frames)
+  }
+  exportfcp (savefunc, frames) {
+    const uuid = require('uuid')
+    const moment = require('moment')
+    const assets = frames.map(frame => {
+      return `<asset id="r${100 + frame.frame}" name="${frame.frame}" uid="${uuid.v4()}" src="file://${frame.path}" start="0s" duration="0s" hasVideo="1" format="r4" />`
+    }).join('\n')
+    let curroffset = 0
+    const videos = frames.map(frame => {
+      const result = `<video name="${frame.frame}" lane="1" offset="${curroffset * 100}/3000s" ref="r${100 + frame.frame}" duration="${frame.duration * 100}/3000s" start="0s"/>`
+      curroffset += frame.duration
+      return result
+    }).join('\n')
+    const totalframe = curroffset
+    const result = `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE fcpxml>
+<fcpxml version="1.8">
+    <resources>
+        <format id="r1" name="FFVideoFormat1080p30" frameDuration="100/3000s" width="1280" height="720" colorSpace="1-1-1 (Rec. 709)"/>
+        <format id="r4" name="FFVideoFormatRateUndefined" width="1280" height="720"/>
+        <effect id="r2" name="自定" uid=".../Generators.localized/Solids.localized/Custom.localized/Custom.motn"/>
+        ${assets}
+    </resources>
+    <library location="file:///Users/wesleywang/Movies/%E9%BB%98%E8%AE%A4.fcpbundle/">
+        <event name="日常" uid="6ABE2EA9-BBC0-4D9C-A187-9CBDB547B8F0">
+            <project name="CaptureJS生成" uid="${uuid.v4()}" modDate="${moment().format('YYYY-MM-DD HH:mm:ss')} +0800">
+                <sequence duration="${totalframe * 100}/3000s" format="r1" tcStart="0s" tcFormat="NDF" audioLayout="stereo" audioRate="48k">
+                    <spine>
+                        <video name="自定" offset="0s" ref="r2" duration="${totalframe * 100}/3000s" start="0s">
+                            ${videos}
+                        </video>
+                    </spine>
+                </sequence>
+            </project>
+        </event>
+    </library>
+</fcpxml>
+`
+    savefunc('video', 'fcpxml', Buffer.from(result, 'utf-8'))
+    // console.log(frames)
+  }
+}
